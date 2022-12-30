c3b7c93a58f8b3747d80637a287fea2ee9d98c08
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-12-30 23:19:40 +0530

    get post

--- index.js
+++ index.js
@@ -20,20 +20,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-async function run(){
-    try{
-        const userDb = client.db('sociel-inr').collection('userall')
+async function run() {
+  try {
+    const myusers = client.db("socielDb").collection("users");
+    const myposts = client.db("socielDb").collection("posts");
+    // user post in dt
+    app.post("/userss", async (req, res) => {
+      const body = req.body;
 
-    // user post in dt 
-    app.post('/userss' , async(req ,res)=>{
-        const body = req.body;
-        console.log(body);
-    })
-    }
-    finally{
+      const result = await myusers.insertOne(body);
+      res.send(result);
+    });
+    // get the display users
+    app.get("/user", async (req, res) => {
+      const email = req.query.email;
+      console.log('email', email )
+      const query = { email: email };
+      const result = await myusers.findOne(query);
+      res.send(result);
+    });
 
-    }
+
+    // posts  in dt
+    app.post("/posts", async (req, res) => {
+      const bodys = req.body;
+      const result = await myposts.insertOne(bodys);
+      res.send(result);
+    });
+    // get the post in user see
+    app.get("/posts", async (req, res) => {
+      const emails = req.query.email;
+      console.log(emails);
+      const query = { email: emails };
+      const result = await myposts.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
 }
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("ok sona");
